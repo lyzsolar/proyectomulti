f3140798c2b49b3f81b16a15b5538477881bdbff
MelissaZarate08 <233313@ids.upchiapas.edu.mx>
2024-03-20 11:44:41 -0600

    Interfaz de login

---
+++ AjalCafe/src/main/java/com/lyzsolar/ajalcafe/controllers/loginController.java
@@ -0,0 +1,20 @@
+package com.lyzsolar.ajalcafe.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class loginController{
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
