be6136a3c57127a8a20ab234a2dadde6f59e2ca1
MelissaZarate08 <233313@ids.upchiapas.edu.mx>
2024-03-23 21:14:47 -0600

    Interfaz login terminada

--- AjalCafe/src/main/java/com/lyzsolar/ajalcafe/controllers/loginController.java
+++ AjalCafe/src/main/java/com/lyzsolar/ajalcafe/controllers/loginController.java
@@ -2,9 +2,13 @@ package com.lyzsolar.ajalcafe.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
-public class loginController{
+public class loginController {
 
     @FXML
     private ResourceBundle resources;
@@ -13,8 +17,26 @@ public class loginController{
     private URL location;
 
     @FXML
+    private Button IngresarButton;
+
+    @FXML
+    private TextField contraseñaTxt;
+
+    @FXML
+    private ComboBox<?> usuarioCombobox;
+
+    @FXML
+    void ingresar_App(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
+        assert IngresarButton != null : "fx:id=\"IngresarButton\" was not injected: check your FXML file 'login-view.fxml'.";
+        assert contraseñaTxt != null : "fx:id=\"contraseñaTxt\" was not injected: check your FXML file 'login-view.fxml'.";
+        assert usuarioCombobox != null : "fx:id=\"usuarioCombobox\" was not injected: check your FXML file 'login-view.fxml'.";
 
     }
 
 }
+
