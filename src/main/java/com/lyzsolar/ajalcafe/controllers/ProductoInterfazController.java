9370e92d01a0fb22128b307aa687da4ff99aaea6
lyzsolar <233432@ids.upchiapas.edu.mx>
2024-03-28 12:24:49 -0600

    interfaz producto

---
+++ src/main/java/com/lyzsolar/ajalcafe/controllers/ProductoInterfazController.java
@@ -0,0 +1,41 @@
+package com.lyzsolar.ajalcafe.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class ProductoInterfazController {
+
+    @FXML
+    private Button agregar;
+
+    @FXML
+    private Button confirmar;
+
+    @FXML
+    private Button eliminar;
+
+    @FXML
+    private Button ver;
+
+    @FXML
+    void onClickedAgregarbuttom(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onClickedConfirmarbuttom(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onClickedEliminarbuttom(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onClickedVerbuttom(MouseEvent event) {
+
+    }
+
+}
