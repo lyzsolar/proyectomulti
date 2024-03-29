8f8134d1cac9e72015a5710742623fd5e6f7ad0a
lyzsolar <233432@ids.upchiapas.edu.mx>
2024-03-29 16:40:54 -0600

    conectados todos los botones del administrador

--- src/main/java/com/lyzsolar/ajalcafe/controllers/adminController.java
+++ src/main/java/com/lyzsolar/ajalcafe/controllers/adminController.java
@@ -1,10 +1,17 @@
 package com.lyzsolar.ajalcafe.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import com.lyzsolar.ajalcafe.App;
+import com.lyzsolar.ajalcafe.models.Producto;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class adminController {
 
@@ -32,33 +39,66 @@ public class adminController {
     @FXML
     private Button cerrarButton;
 
+    Stage producto = new Stage();
+    Stage pedido = new Stage();
+    Stage menu = new Stage();
+    Stage venta = new Stage();
+    Stage reporte = new Stage();
+
     @FXML
     void OnMouseclickedCerrarButton(MouseEvent event) {
+        Stage stage = (Stage) cerrarButton.getScene().getWindow();
+        stage.close();
 
     }
 
     @FXML
-    void OnMouseclickedMenuButton(MouseEvent event) {
+    void OnMouseclickedMenuButton(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("MenuInterfaz-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        menu.setTitle("Menú");
+        menu.setScene(scene);
+        menu.show();
 
     }
 
     @FXML
-    void OnMouseclickedPedidoButton(MouseEvent event) {
+    void OnMouseclickedPedidoButton(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("PedidoInterfaz-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        pedido.setTitle("Menú pedido");
+        pedido.setScene(scene);
+        pedido.show();
 
     }
 
     @FXML
-    void OnMouseclickedProductoButton(MouseEvent event) {
+    void OnMouseclickedProductoButton(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("ProductoInterfaz-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        producto.setTitle("Menú producto");
+        producto.setScene(scene);
+        producto.show();
 
     }
 
     @FXML
-    void OnMouseclickedReporteButton(MouseEvent event) {
+    void OnMouseclickedReporteButton(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("ReporteInterfaz-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        reporte.setTitle("Menú producto");
+        reporte.setScene(scene);
+        reporte.show();
 
     }
 
     @FXML
-    void OnMouseclickedVentasButton(MouseEvent event) {
+    void OnMouseclickedVentasButton(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("VentasInterfaz-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        venta.setTitle("Menú producto");
+        venta.setScene(scene);
+        venta.show();
 
     }
 
