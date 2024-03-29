27ec357782d88bfa6aba5adb202119a0717ecd35
lyzsolar <233432@ids.upchiapas.edu.mx>
2024-03-29 14:38:27 -0600

    home, administrador y vendedor conectados

--- src/main/java/com/lyzsolar/ajalcafe/controllers/homeController.java
+++ src/main/java/com/lyzsolar/ajalcafe/controllers/homeController.java
@@ -1,12 +1,19 @@
 package com.lyzsolar.ajalcafe.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import com.lyzsolar.ajalcafe.App;
+import com.lyzsolar.ajalcafe.models.Usuario;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class homeController {
 
@@ -21,20 +28,38 @@ public class homeController {
 
     @FXML
     private PasswordField password;
-
+    Usuario administrador = new Usuario();
+    Usuario vendedor = new Usuario();
+    Stage callMenu = new Stage();
+    Stage menuAdministrador = new Stage();
+    Stage menuVendedor = new Stage();
     @FXML
     private TextField user;
 
     @FXML
     void OnMouseclickedIngresarButton(MouseEvent event) {
+        String nombreUsuario = user.getText();
+        String contrasena = password.getText();
 
+        try{
+            if (nombreUsuario.equals(administrador.getUsuario()) && contrasena.equals(administrador.getContrasena())){
+                FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Admin-view.fxml"));
+                Scene scene = new Scene(fxmlLoader.load());
+                callMenu.setTitle("Menu Administrador");
+                callMenu.setScene(scene);
+                callMenu.show();
+            } else if (nombreUsuario.equals(vendedor.getUsuario2()) && contrasena.equals(vendedor.getContrasena2())){
+                FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Vendedor_view.fxml"));
+                Scene scene = new Scene(fxmlLoader.load());
+                menuVendedor.setTitle("Menu Vendedor");
+                menuVendedor.setScene(scene);
+                menuVendedor.show();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-
     @FXML
     void initialize() {
-        assert IngresarButton != null : "fx:id=\"IngresarButton\" was not injected: check your FXML file 'Home-view.fxml'.";
-        assert password != null : "fx:id=\"password\" was not injected: check your FXML file 'Home-view.fxml'.";
-        assert user != null : "fx:id=\"user\" was not injected: check your FXML file 'Home-view.fxml'.";
-
     }
 }
---
+++ src/main/java/com/lyzsolar/ajalcafe/models/Usuario.java
@@ -0,0 +1,24 @@
+package com.lyzsolar.ajalcafe.models;
+
+public class Usuario {
+    private String usuario = "administrador";
+    private String contrasena = "123456";
+    private String usuario2 = "vendedor";
+    private String contrasena2 = "123456";
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public String getUsuario2() {
+        return usuario2;
+    }
+
+    public String getContrasena2() {
+        return contrasena2;
+    }
+}
