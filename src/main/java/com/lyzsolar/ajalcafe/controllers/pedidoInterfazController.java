c1c281218b9364a9be1d0878d7dd0ee6b8aa5e72
lyzsolar <233432@ids.upchiapas.edu.mx>
2024-03-29 22:22:02 -0600

    interfaz pedido pero no conectada

--- src/main/java/com/lyzsolar/ajalcafe/controllers/pedidoInterfazController.java
+++ src/main/java/com/lyzsolar/ajalcafe/controllers/pedidoInterfazController.java
@@ -1,4 +1,68 @@
 package com.lyzsolar.ajalcafe.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 public class pedidoInterfazController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button agregarButton;
+
+    @FXML
+    private Button modificarButton;
+
+    @FXML
+    private Button eliminarButton;
+
+    @FXML
+    private Button regresarButton;
+
+    @FXML
+    void OnMouseclickedAgregarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedEliminarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedModificarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedRegresarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedVerButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void verButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert agregarButton != null : "fx:id=\"agregarButton\" was not injected: check your FXML file 'PedidoInterfaz-view.fxml'.";
+        assert modificarButton != null : "fx:id=\"modificarButton\" was not injected: check your FXML file 'PedidoInterfaz-view.fxml'.";
+        assert eliminarButton != null : "fx:id=\"eliminarButton\" was not injected: check your FXML file 'PedidoInterfaz-view.fxml'.";
+        assert regresarButton != null : "fx:id=\"regresarButton\" was not injected: check your FXML file 'PedidoInterfaz-view.fxml'.";
+
+    }
 }
