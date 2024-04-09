5df4878fb901e2d50f2ced4a77e9af44c481c509
lyzsolar <233432@ids.upchiapas.edu.mx>
2024-04-09 02:51:47 -0600

    interfaz agregar venta

---
+++ src/main/java/com/lyzsolar/ajalcafe/controllers/AgregarVentaController.java
@@ -0,0 +1,52 @@
+package com.lyzsolar.ajalcafe.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class AgregarVentaController {
+
+    @FXML
+    private Button guardarbutton;
+
+    @FXML
+    private Button regresarButton;
+
+    @FXML
+    private ImageView guardarIcono;
+
+    @FXML
+    private ImageView regresarIcono;
+
+    @FXML
+    private TextField idText;
+
+    @FXML
+    private TextField fechaText;
+
+    @FXML
+    private TextField totalText;
+
+    @FXML
+    void OnMouseclickedGuardarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedGuardarIcono(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedRegresarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedRegresarIcono(MouseEvent event) {
+
+    }
+
+}
