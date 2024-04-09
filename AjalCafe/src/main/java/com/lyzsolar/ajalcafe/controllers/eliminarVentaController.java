439c8ade5cb48e6dc2ff6e41c564daaee54942ad
MelissaZarate08 <233313@ids.upchiapas.edu.mx>
2024-04-09 13:00:41 -0600

    Vista EliminarVenta creada

---
+++ AjalCafe/src/main/java/com/lyzsolar/ajalcafe/controllers/eliminarVentaController.java
@@ -0,0 +1,82 @@
+package com.lyzsolar.ajalcafe.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class eliminarVentaController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button BuscarButton;
+
+    @FXML
+    private TextField IDText;
+
+    @FXML
+    private Button eliminarButton;
+
+    @FXML
+    private ImageView eliminarIcono;
+
+    @FXML
+    private Button regresarButton;
+
+    @FXML
+    private ImageView regresarIcono;
+
+    @FXML
+    private ImageView verIcono;
+
+    @FXML
+    void OnMouseclickedBuscarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedEliminarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedEliminarIcono(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedRegresarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedRegresarIcono(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedVerIcono(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert BuscarButton != null : "fx:id=\"BuscarButton\" was not injected: check your FXML file 'EliminarVenta-view.fxml'.";
+        assert IDText != null : "fx:id=\"IDText\" was not injected: check your FXML file 'EliminarVenta-view.fxml'.";
+        assert eliminarButton != null : "fx:id=\"eliminarButton\" was not injected: check your FXML file 'EliminarVenta-view.fxml'.";
+        assert eliminarIcono != null : "fx:id=\"eliminarIcono\" was not injected: check your FXML file 'EliminarVenta-view.fxml'.";
+        assert regresarButton != null : "fx:id=\"regresarButton\" was not injected: check your FXML file 'EliminarVenta-view.fxml'.";
+        assert regresarIcono != null : "fx:id=\"regresarIcono\" was not injected: check your FXML file 'EliminarVenta-view.fxml'.";
+        assert verIcono != null : "fx:id=\"verIcono\" was not injected: check your FXML file 'EliminarVenta-view.fxml'.";
+
+    }
+
+}
