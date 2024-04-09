82f2dafa5b4193edd7901256361bc043d8b210ad
lyzsolar <233432@ids.upchiapas.edu.mx>
2024-04-09 01:52:21 -0600

    interfaz modificar menu

---
+++ src/main/java/com/lyzsolar/ajalcafe/controllers/ModificarMenuController.java
@@ -0,0 +1,68 @@
+package com.lyzsolar.ajalcafe.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class ModificarMenuController {
+
+    @FXML
+    private Button guardarbutton;
+
+    @FXML
+    private Button regresarButton;
+
+    @FXML
+    private ImageView guardarIcono;
+
+    @FXML
+    private ImageView regresarIcono;
+
+    @FXML
+    private TextField idText;
+
+    @FXML
+    private TextField nombreText;
+
+    @FXML
+    private TextField descripcionText;
+
+    @FXML
+    private TextField precioText;
+
+    @FXML
+    private Button buscarrButton;
+
+    @FXML
+    void OnMouseclickedBuscarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedBuscarIcono(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedGuardarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedGuardarIcono(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedRegresarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedRegresarIcono(MouseEvent event) {
+
+    }
+
+}
