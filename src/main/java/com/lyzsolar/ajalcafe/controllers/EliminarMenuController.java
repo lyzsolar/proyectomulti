1072ad66b4c78a57d2aec48ebd4496ffb199fa3e
lyzsolar <233432@ids.upchiapas.edu.mx>
2024-04-09 02:19:47 -0600

    inerfaz eliminar menu

---
+++ src/main/java/com/lyzsolar/ajalcafe/controllers/EliminarMenuController.java
@@ -0,0 +1,62 @@
+package com.lyzsolar.ajalcafe.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class EliminarMenuController {
+
+    @FXML
+    private ImageView eliminarIcono;
+
+    @FXML
+    private TextField NombreText;
+
+    @FXML
+    private Button eliminarButton;
+
+    @FXML
+    private Button BuscarButton;
+
+    @FXML
+    private Button regresarButton;
+
+    @FXML
+    private ImageView regresarIcono;
+
+    @FXML
+    private ImageView buscarIcono;
+
+    @FXML
+    void OnMouseclickedBuscarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedBuscarIcono(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedEliminarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedEliminarIcono(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedRegresarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseclickedRegresarIcono(MouseEvent event) {
+
+    }
+
+}
